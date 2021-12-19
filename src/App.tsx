56a924d8fe68db6841da27c4892ff77829a249fb
nastassia <anastasiyamihalenko@gmail.com>
2021-12-20 00:57:06 +0300

    2 files modified(button, select done), 4 files added(pictures)

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,37 @@ import pic5 from './assets/pic5.png';
 import pic6 from './assets/pic6.png';
 import pic7 from './assets/pic7.png';
 import pic8 from './assets/pic8.png';
+import pic10 from './assets/pic10.png';
+import pic11 from './assets/pic11.png';
+
+
+import find from './assets/find.png';
 
 type initialItemType = {
     id: number
-    img: string
+    img: any
+    index?: number
 }
 
 function App() {
     const initialState: Array<initialItemType> = [
         {id: 1, img: pic1},
         {id: 2, img: pic2},
-        {id: 1, img: pic3},
-        {id: 2, img: pic4},
-        {id: 1, img: pic5},
-        {id: 2, img: pic6},
-        {id: 2, img: pic7},
-        {id: 2, img: pic8},
+        {id: 3, img: pic3},
+        {id: 4, img: pic4},
+        {id: 5, img: pic5},
+        {id: 6, img: pic6},
+        {id: 7, img: pic7},
+        {id: 8, img: pic8},
+        {id: 9, img: pic10},
+        {id: 10, img: pic11},
     ]
     const doubleArray = [...initialState, ...initialState];
 
     const [cards, setCards] = useState<Array<initialItemType>>([]);
+    const [openedCards, setOpenedCards] = useState<Array<number>>([])
+    const [matched, setMatched] = useState<Array<number>>([])
+    const [valueSelect, setValueSelect] = useState<string>("Hard")
 
     const shuffle = (array: any) => {
         let currentIndex = array.length, temporaryValue, randomIndex;
@@ -47,21 +58,98 @@ function App() {
         return setCards(shuffle(doubleArray));
     }, []);
 
+    const onClickHandler = (index: number) => {
+        setOpenedCards([...openedCards, index])
+    }
+
+    useEffect(() => {
+        if (openedCards.length > 2) {
+            openedCards.splice(1, 2)
+        }
+
+        if (openedCards.length < 2)
+            return
+        const first = cards[openedCards[0]]
+        const second = cards[openedCards[1]]
+
+        if (second && first.id === second.id) {
+            setMatched([...matched, first.id])
+        }
+
+        if (openedCards.length === 2 && first !== second) setTimeout(() => setOpenedCards([]), 1000)
+
+    }, [openedCards])
+
+    console.log(openedCards)
+    console.log(matched)
+
+    useEffect(() => {
+        if (valueSelect === "Easy") {
+            initialState.splice(1, 4)
+            updateArray()
+        }
+        if (valueSelect === "Middle") {
+            initialState.splice(1, 2)
+            updateArray()
+        }
+        if (valueSelect === "Hard") {
+            updateArray()
+        }
+
+    }, [valueSelect])
+
+    const onChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setValueSelect(e.currentTarget.value)
+    }
+
+    const classNameHard = valueSelect === "Hard" ? "cardsHard" : "cards";
+
+    const updateArray = () => {
+        const doubleArray = [...initialState, ...initialState];
+        setCards(shuffle(doubleArray));
+        setOpenedCards([])
+        setMatched([])
+    }
+
+    const onClickRestart = () => {
+        setOpenedCards([])
+        setMatched([])
+    }
     return (
-        <>
-            <div className={"picContainer"}>
-                {cards.map(m => {
-                        return (
-                            <div>
-                                <img
-                                    onClick={() => {}}
-                                    src={m.img} alt={"#"}/>
+        <div className={"appContainer"}>
+            <select value={valueSelect}
+                    onChange={onChange}>
+                <option disabled={true}>Level</option>
+                <option value={"Easy"} id={"1"}>Easy</option>
+                <option value={"Middle"} id={"2"}>Middle</option>
+                <option value={"Hard"} id={"3"} selected>Hard</option>
+            </select>
+            <div className={classNameHard}>
+                {cards.map((m, index) => {
+                    let isFlipped = false
+                    if (openedCards.includes(index)) isFlipped = true
+                    if (matched.includes(m.id)) isFlipped = true
+
+                    return (
+                        <div
+                            onClick={() => onClickHandler(index)}
+                            className={isFlipped ? "card" + " isFlipped" : "card"}
+                        >
+                            <div className={"inner"}>
+                                <div className={"front"}>
+                                    <img
+                                        src={m.img} alt={"#"}/>
+                                </div>
+                                <div className={"back"}>
+                                    <img src={find} alt={"#"}/>
+                                </div>
                             </div>
-                        )
-                    }
-                )}
+                        </div>
+                    )
+                })}
             </div>
-        </>
+            <button onClick={onClickRestart}>Restart</button>
+        </div>
     );
 }
 
